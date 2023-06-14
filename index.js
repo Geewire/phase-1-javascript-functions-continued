b8d94bd6629e09e082576244c396f1d35cb74541
root <root@DESKTOP-6I3RBRC>
2023-06-14 10:50:47 +0300

    First Commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+ return `This Saturday, I want to ${activity}!`; 
+}
+saturdayFun();
+
+    
+function mondayWork(activity= "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+
+function wrapAdjective(boom= '*'){
+    return function(flair= "special"){
+        return `You are ${boom}${flair}${boom}!`;
+
+    }
+ 
+}
+
+    
